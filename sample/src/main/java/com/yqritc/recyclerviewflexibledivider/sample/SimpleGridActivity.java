67b192b0a4a01a33e15c76d1ec6e011da6df49b9
chaunmi <719589905@qq.com>
2023-03-08 14:21:11 +0800

    迁移到androidx

--- sample/src/main/java/com/yqritc/recyclerviewflexibledivider/sample/SimpleGridActivity.java
+++ sample/src/main/java/com/yqritc/recyclerviewflexibledivider/sample/SimpleGridActivity.java
@@ -5,12 +5,12 @@ import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.OrientationHelper;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class SimpleGridActivity extends AppCompatActivity {
@@ -28,7 +28,7 @@ public class SimpleGridActivity extends AppCompatActivity {
 
         SimpleAdapter adapter = new SimpleAdapter(this);
         GridLayoutManager manager = new GridLayoutManager(this, 3);
-        manager.setOrientation(OrientationHelper.VERTICAL);
+        manager.setOrientation(RecyclerView.VERTICAL);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_recyclerview);
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
